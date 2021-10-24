f5703cde010748876c6d8527edea5f92f73640f5
Zeeshan Hooda <zeeshan.hooda@smartaccess.io>
2021-10-24 15:58:14 -0600

    Write to VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello, World!";
+
 // this function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,5 +15,15 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     // this is the entry point since the linker looks for
     // a function `_start` by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
+        }
+    }
+    
     loop {}
 }

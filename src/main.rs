2ca10ecda5949d9a9577755d2330135c0345ca70
Zeeshan Hooda <zeeshan.hooda@smartaccess.io>
2021-10-25 20:25:53 -0600

    Implemented integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable Rust main fn entry point
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(very_bad_kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
-
-// this function is called on panic
-#[cfg(not(test))] // use this panic handler on non test builds
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)] // use this panic handler on non test builds
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
+use very_bad_kernel::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -39,50 +20,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-// Exiting QEMU (mainly for ease of testing)
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
+// this function is called on panic
+#[cfg(not(test))] // use this panic handler on non test builds
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    // Self explanatory lol
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+#[cfg(test)] // use this panic handler on non test builds
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    very_bad_kernel::test_panic_handler(info)
 }

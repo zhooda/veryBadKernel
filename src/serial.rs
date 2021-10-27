c1e23914e0ec4387d478d221877a52dc5ebe4e66
Zeeshan Hooda <zeeshan.hooda@smartaccess.io>
2021-10-26 20:16:13 -0600

    Better docs

--- src/serial.rs
+++ src/serial.rs
@@ -2,9 +2,9 @@ use uart_16550::SerialPort;
 use spin::Mutex;
 use lazy_static::lazy_static;
 
-// Using lazy_static to ensure the init method 
-// is called EXACTLY once on its first use
 lazy_static! {
+    /// A global serial port instance used to print 
+    /// to the host through the serial interface
     pub static ref SERIAL1: Mutex<SerialPort> = {
         // COM1 <=> IO Port 0x3F8
         let mut serial_port = unsafe { SerialPort::new(0x3F8) };
@@ -13,15 +13,15 @@ lazy_static! {
     };
 }
 
-// Abstractions to make the serial port easy to use!
-
+/// Prints the given formatted string to the host
+/// through the global `SERIAL1` instance
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
     SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
 }
 
-// Prints to the host through serial interface
+/// Prints to the host through serial interface
 #[macro_export]
 macro_rules! serial_print {
     ($($arg:tt)*) => {
@@ -29,7 +29,7 @@ macro_rules! serial_print {
     };
 }
 
-// Prints to the host through serial interface, now with newlines!
+/// Prints to the host through serial interface, now with newlines!
 #[macro_export]
 macro_rules! serial_println {
     () => ($crate::serial_print!("\n"));
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -3,6 +3,17 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
 
+lazy_static! {
+    /// A global `Writer` instance that can be used for writing
+    /// to the VGA text buffer. Used by `print!` and `println!` macros
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightRed, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+// Standard color pallette in VGA text mode
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -25,16 +36,20 @@ pub enum Color {
     White = 15,
 }
 
+/// Combination of a foreground and background color
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
 struct ColorCode(u8);
 
 impl ColorCode {
+    /// Create a new `ColorCode` with the given foreground and background codes
     fn new(foreground: Color, background: Color) -> ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
 
+/// A screen character in the VGA text buffer
+/// consisting of an ASCII char and color code
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
@@ -42,14 +57,21 @@ struct ScreenChar {
     color_code: ColorCode,
 }
 
+/// The height of the text buffer (generally 25 lines)
 const BUFFER_HEIGHT: usize = 25;
+/// The width of the text buffer (generally 80 columns)
 const BUFFER_WIDTH: usize = 80;
 
+/// A struct representing the VGA text buffer
 #[repr(transparent)]
 struct Buffer {
     chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
+/// A writer type that allows writing ASCII bytes and strings
+/// to an underlying `Buffer`. Wraps lines at `BUFFER_WIDTH`.
+/// Supports newline characters and implements the
+/// `core::fmt::Write` trait
 pub struct Writer {
     column_position: usize,
     color_code: ColorCode,
@@ -57,6 +79,8 @@ pub struct Writer {
 }
 
 impl Writer {
+    /// Writes an ASCII byte to the buffer.
+    /// Wraps lines at `BUFFER_WIDTH`. Supports `\n` newline char.
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -78,6 +102,7 @@ impl Writer {
         }
     }
 
+    /// Shifts all lines one line up and clears the last row
     fn new_line(&mut self) {
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
@@ -89,6 +114,7 @@ impl Writer {
         self.column_position = 0;
     }
 
+    /// Clears a row by overwriting it with blank chars
     fn clear_row(&mut self, row: usize) {
         let blank = ScreenChar {
             ascii_character: b' ',
@@ -99,6 +125,10 @@ impl Writer {
         }
     }
 
+    /// Writes the given ASCII string to the buffer.
+    /// Wraps lines at `BUFFER_WIDTH`. Supports the '\n' newline char.
+    /// Does **not** support strings with non-ASCII chars
+    /// since they can't be printed in the VGA test mode
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
@@ -118,26 +148,23 @@ impl fmt::Write for Writer {
     }
 }
 
-lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::LightRed, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    });
-}
-
-// print and println macros
+/// Like the `print!` macro in the standard library, but prints
+/// to the VGA text buffer
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
 }
 
+/// Like the `println!` macro in the standard library, but prints
+/// to the VGA text buffer
 #[macro_export]
 macro_rules! println {
     () => ($crate::print!("\n"));
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
 
+/// Prints the given formatted string to the VGA text buffer
+/// through the global `WRITER` instance
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
